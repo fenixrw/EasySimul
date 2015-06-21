ac62a3eff48e619eca3080da74f86fd6dfb23f5c
feliperw <feliperw@msn.com>
2015-06-21 07:35:09 -0300

    Simulation results with documents

---
+++ EasySimul/Release/run - PaperTests.bat
@@ -0,0 +1,15 @@
+EasySimul.exe -f Cenario1.txt
+EasySimul.exe -f Cenario2.txt -a
+EasySimul.exe -f Cenario3.txt -o
+EasySimul.exe -f Cenario4.txt -i -o
+EasySimul.exe -f Cenario5.txt -i -o --rule
+EasySimul.exe -f Cenario1r100.txt -r 100
+EasySimul.exe -f Cenario2r100.txt -a -r 100
+EasySimul.exe -f Cenario3r100.txt -o -r 100
+EasySimul.exe -f Cenario4r100.txt -i -o -r 100
+EasySimul.exe -f Cenario5r100.txt -i -o --rule -r 100
+EasySimul.exe -f Cenario1r1000.txt -r 1000
+EasySimul.exe -f Cenario2r1000.txt -a -r 1000
+EasySimul.exe -f Cenario3r1000.txt -o -r 1000
+EasySimul.exe -f Cenario4r1000.txt -i -o -r 1000
+EasySimul.exe -f Cenario5r1000.txt -i -o --rule -r 1000
